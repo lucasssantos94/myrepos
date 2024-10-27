1adaeebe673500b4259fa2b62a203f889f7d697b
lucasssantos94 <lucas.ssantos94@outlook.com>
2024-10-27 18:31:23 -0300

    stylized main

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,7 +1,21 @@
-import * as Styles from './styles.js';
+import * as Styles from "./styles.js";
+import {FaGithub, FaPlus} from 'react-icons/fa'
 
 export default function Main() {
-    return (        
-            <Styles.Title>Main</Styles.Title>
-    )
+  return (
+    <Styles.Container>
+        <h1>
+            <FaGithub size={30}/>
+            Meus Repositórios
+        </h1>
+
+        <Styles.Form onSubmit={() => {}}>
+            <input type="text" placeholder="Digite o nome do repositório"/>
+
+            <Styles.SubmitButton type="submit">
+                <FaPlus size={14} color="#fff"/>
+            </Styles.SubmitButton>
+        </Styles.Form>
+    </Styles.Container>
+  )
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,6 +1,47 @@
 import styled from "styled-components";
 
-export const Title = styled.h1`
-    color: #f00;
+
+export const Container = styled.div`
+    max-width: 700px;
+    background: #FFF;
+    border-radius: 4px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+    padding: 30px;
+    margin: 80px auto;
+    
+    h1 {
+        font-size: 30px;
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        gap: 10px;
+       
+    }
+`;
+
+export const Form = styled.form`
+    margin-top: 30px;
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    input {
+        flex: 1;
+        border: 1px solid #DDD;
+        padding: 10px 15px;
+        border-radius: 4px;
+        font-size: 18px;
+    }
+`;
+
+export const SubmitButton = styled.button`
+    background: #0D2636;
+    border: 0;
+    padding: 0 15px;
+    border-radius: 4px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #FFF;
+    font-weight: bold;   
 `;
 
